ccb917abcfebf13ee0d2beeeb3f6be0bb0c8648c
Tosla028 <tosla028@users.noreply.github.com>
2026-08-29 21:55:36 +0700

    change phrases

--- src/data/phrases.js
+++ src/data/phrases.js
@@ -1,14 +1,6 @@
 export const phrases = [
-  'Terima kasih',
-  'atas lelah',
-  'dan senyummu',
-  'selama ini.',
-  'Jejak yang kalian tinggalkan',
-  'akan kami lanjutkan.',
-  'Semoga,',
-  'perjalanan kalian',
-  'selalu',
-  'dipenuhi kebaikan.',
-  'Dari kami,', 
-  'kepengurusan Tosla 028 \u{1F499}\u{2764}\u{FE0F}',
+  'Terima kasih atas lelah dan senyummu selama ini.',
+  'Jejak yang kalian tinggalkan akan kami lanjutkan.',
+  'Semoga perjalanan kalian selalu dipenuhi kebaikan.',
+  'Dari kami, kepengurusan Tosla 028 💙❤️'
 ]
